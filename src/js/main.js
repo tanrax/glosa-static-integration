c83e2c1a4f93139dfe2797d5a319361fe1bc1708
Andros Fenollosa <andros@fenollosa.email>
2020-04-08 23:23:56 +0200

    Update styles

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,11 @@ new Vue({
     mounted: function () {
         this.getComments();
     },
+    computed: {
+        commentsParent: function() {
+            return this.comments.filter(item => item.parent === '');
+        }
+    },
     methods: {
         getComments: function () {
             axios({
@@ -25,6 +30,19 @@ new Vue({
         },
         getURL: function () {
             return 'https://programadorwebvalencia.com/cita-de-la-semana-7/';
+        },
+        formatDate: function (unixtime) {
+            const DATE = new Date(unixtime * 1000);
+            return `${DATE.getDate()}/${DATE.getMonth() + 1}/${DATE.getFullYear()}`;
+        },
+        filterHTMLTags: function (text) {
+            let myElement = document.createElement('div');
+            myElement.innerHTML = text;
+            return myElement.textContent;
+        },
+        openNewComment: function (parent = '') {
+            this.showNewComment = true;
+            console.log(parent)
         }
     }
 });
