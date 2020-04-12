1cd52b3e3ed82dd4e8f03c556509ed48a6572080
Andros Fenollosa <andros@fenollosa.email>
2020-04-12 18:00:00 +0200

    Fix scroll in mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,15 @@
 const URL_API = 'http://programadorwebvalencia.localhost:4000/api/v1';
 const ENDPOINT_COMMENTS_API = '/comments/';
 
-Vue.component('comment', {
-    props: {
-        id: Number,
-        comments: Object
-    },
-    data: function () {
-        return {
-            maxDeep: 4
-        };
-    },
-    template: document.querySelector('template#comment').innerHTML,
-    computed: {
-        fields: function () {
-            return R.head(R.filter((item) => item.id === this.id, this.comments));
-        },
-        childs: function () {
-            return R.filter(item => item.parent === this.id, this.comments);
-        }
-    },
-    methods: {
-        formatDate: function (unixtime) {
-            const DATE = new Date(unixtime * 1000);
-            return `${DATE.getDate()}/${DATE.getMonth() + 1}/${DATE.getFullYear()}`;
-        },
-        filterHTMLTags: function (text) {
-            let myElement = document.createElement('div');
-            myElement.innerHTML = text;
-            return myElement.textContent;
-        },
-        openNewComment: function (id) {
-            this.$emit('openComment', id);
-        }
-    }
-});
-
-new Vue({
+/**
+  * Vue APP
+ **/
+let app = new Vue({
     el: '#app-comments',
     data: {
         comments: [],
-        showNewComment: false
+        showNewComment: false,
+        reply: undefined
     },
     mounted: function () {
         this.getComments();
@@ -66,16 +35,54 @@ new Vue({
         getCommentsChilds: function (id) {
             return R.filter(item => item.parent === id, this.comments);
         },
-        getSingleComment: function (id) {
-            return R.head(R.filter((item) => item.id === id, this.comments));
-        },
         getURL: function () {
             return 'https://programadorwebvalencia.com/cual-es-el-mejor-navegador-web-2020/';
         },
-        openNewComment: function (id) {
+        openNewComment: function (id = undefined) {
+            this.reply = id;
             this.showNewComment = true;
-            console.log(id);
+        }
+    }
+});
 
+/**
+ * Vue component: comment
+ **/
+Vue.component('comment', {
+    props: {
+        id: Number,
+        comments: Object
+    },
+    data: function () {
+        return {
+            maxDeep: 4,
+            maxLengthAuthor: 15
+        };
+    },
+    template: document.querySelector('template#comment').innerHTML,
+    computed: {
+        fields: function () {
+            return R.head(R.filter((item) => item.id === this.id, this.comments));
+        },
+        childs: function () {
+            return R.filter(item => item.parent === this.id, this.comments);
+        }
+    },
+    methods: {
+        formatDate: function (unixtime) {
+            const DATE = new Date(unixtime * 1000);
+            return `${DATE.getDate()}/${DATE.getMonth() + 1}/${DATE.getFullYear()}`;
+        },
+        formatEllipsisAuthor: function (text) {
+            return text.length > this.maxLengthAuthor ? `${text.slice(0, this.maxLengthAuthor - 3)}...` : text;
+        },
+        filterHTMLTags: function (text) {
+            let myElement = document.createElement('div');
+            myElement.innerHTML = text;
+            return myElement.textContent;
+        },
+        openReply: function (id) {
+            app.openNewComment(id);
         }
     }
 });
