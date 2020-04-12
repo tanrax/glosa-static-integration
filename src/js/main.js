fc1ca9b09f2fdb8b72f26d6d2c1523931a2c64c1
Andros Fenollosa <andros@fenollosa.email>
2020-04-12 11:18:17 +0200

    Add component

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,33 @@
 const URL_API = 'http://programadorwebvalencia.localhost:4000/api/v1';
 const ENDPOINT_COMMENTS_API = '/comments/';
 
+Vue.component('comment', {
+    props: ['fields'],
+    template: document.querySelector('template#comment').innerHTML,
+    methods: {
+        formatDate: function (unixtime) {
+            const DATE = new Date(unixtime * 1000);
+            return `${DATE.getDate()}/${DATE.getMonth() + 1}/${DATE.getFullYear()}`;
+        },
+        filterHTMLTags: function (text) {
+            let myElement = document.createElement('div');
+            myElement.innerHTML = text;
+            return myElement.textContent;
+        },
+    }
+});
+
 new Vue({
     el: '#app-comments',
     data: {
         comments: [],
         showNewComment: false,
-        parents: [],
         maxDeep: 4
     },
     mounted: function () {
         this.getComments();
     },
     computed: {
-        commentsSortWithChilds: function() {
-            return this.comments.filter(item => this.getDeep(item.id) === 0).map(item => [].concat(item, this.getCommentsChilds(item.id, 1))).flat();
-        }
     },
     methods: {
         getComments: function () {
@@ -36,39 +48,8 @@ new Vue({
         getSingleComment: function (id) {
             return R.head(R.filter((item) => item.id === id, this.comments));
         },
-        getDeep: function (id) {
-            // Get commet by id
-            const COMMENT = this.getSingleComment(id);
-            if (!R.isNil(COMMENT)) {
-                // Parent
-                if (R.isNil(COMMENT.parent)) {
-                    // Count deep
-                    const totalDeep = R.length(this.parents);
-                    // Reset parents
-                    this.parents = [];
-                    // Return deep
-                    return totalDeep;
-                } else {
-                    // Calculate deep
-                    //// Add id to parents
-                    this.parents = R.append(id, this.parents);
-                    //// Search next parent
-                    return this.getDeep(COMMENT.parent);
-                }
-            }
-            return undefined;
-        },
         getURL: function () {
             return 'https://programadorwebvalencia.com/cual-es-el-mejor-navegador-web-2020/';
-        },
-        formatDate: function (unixtime) {
-            const DATE = new Date(unixtime * 1000);
-            return `${DATE.getDate()}/${DATE.getMonth() + 1}/${DATE.getFullYear()}`;
-        },
-        filterHTMLTags: function (text) {
-            let myElement = document.createElement('div');
-            myElement.innerHTML = text;
-            return myElement.textContent;
         },
         openNewComment: function (parent = '') {
             this.showNewComment = true;
